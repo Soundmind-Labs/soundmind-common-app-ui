d01163eee0fd9326aec77dbfd8568eedd2640588
hskim <hskim@soundmind.life>
2025-12-22 15:47:40 +0900

    chore: update yarn.lock for CI

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,16 +1,22 @@
 const path = require('path');
-const { getConfig } = require('react-native-builder-bob/babel-config');
-const pkg = require('../package.json');
-
-const root = path.resolve(__dirname, '..');
+const pak = require('../package.json');
 
 module.exports = function (api) {
   api.cache(true);
 
-  return getConfig(
-    {
-      presets: ['babel-preset-expo'],
-    },
-    { root, pkg }
-  );
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+          alias: {
+            // [수정됨] pak.source가 없어서 에러나던 것을 'src'로 직접 연결
+            [pak.name]: path.join(__dirname, '../src'),
+          },
+        },
+      ],
+    ],
+  };
 };
